f57abf525a7a55c7b5859e5866c82c6b5e1cffd1
tastaslim <taslimarif600@gmail.com>
2020-09-02 12:42:40 +0530

    Added Hashing

--- Data Structures and Algorithms/DP/1.js
+++
@@ -1,5 +0,0 @@
-var a = function sum(a, b) {
-	return a + b;
-};
-
-console.log(a(1, 2));
--- Data Structures and Algorithms/Tree/DFS-PostOrderTraversal.cpp
+++ Data Structures and Algorithms/Tree/DFS-PostOrderTraversal.cpp
@@ -30,20 +30,49 @@ TreeNode<int> *takeInput()
     }
     return root;
 }
-
-void Preorder(TreeNode<int> *root)
+// DFS ---> Recursive
+/*
+void postOrder(TreeNode<int> *root)
 {
     if (root == NULL)
         return;
     for (int i = 0; i < root->children.size(); i++)
     {
-        Preorder(root->children[i]);
+        postOrder(root->children[i]);
     }
     cout << root->data << " ";
+}
+*/
+
+// BFS ---> Iterative approach
+void postOrder(TreeNode<int> *root)
+{
+    if (root == NULL)
+        return;
+    stack<TreeNode<int> *> s1;
+    s1.push(root);
+    stack<TreeNode<int> *> s2;
+    while (!s1.empty())
+    {
+        TreeNode<int> *topper = s1.top();
+        s2.push(topper);
+        s1.pop();
+        for (int i = 0; i < topper->children.size(); i++)
+        {
+            s1.push(topper->children[i]);
+        }
+    }
+
+    while (!s2.empty())
+    {
+        TreeNode<int> *ans = s2.top();
+        cout << ans->data << " ";
+        s2.pop();
+    }
 }
 int main()
 {
     TreeNode<int> *root = takeInput();
-    Preorder(root);
+    postOrder(root);
     return 0;
 }
